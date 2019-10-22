3ce545cf63eac8ab3a639c84d349d52b43051fa8
Webster Zhang <wencatzhang@gmail.com>
2019-10-22 17:53:28 +1100

    Optimised function name

--- implementation/BeautifulPrint.py
+++ implementation/BeautifulPrint.py
@@ -13,56 +13,56 @@ class BeautifulPrint:
     UNDERLINE = '\033[4m'
 
     @staticmethod
-    def printWarning(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
+    def warning(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
         print(BeautifulPrint.YELLOW, end='')
         print(msg, sep=sep, end=end, file=file, flush=flush)
         print(BeautifulPrint.END_PRINT, end='')
 
     @staticmethod
-    def printInfoBlue(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
+    def infoBlue(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
         print(BeautifulPrint.BLUE, end='')
         print(msg, sep=sep, end=end, file=file, flush=flush)
         print(BeautifulPrint.END_PRINT, end='')
 
     @staticmethod
-    def printInfoPurple(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
+    def infoPurple(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
         print(BeautifulPrint.PURPLE, end='')
         print(msg, sep=sep, end=end, file=file, flush=flush)
         print(BeautifulPrint.END_PRINT, end='')
 
     @staticmethod
-    def printError(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
+    def error(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
         print(BeautifulPrint.RED, end='')
         print(msg, sep=sep, end=end, file=file, flush=flush)
         print(BeautifulPrint.END_PRINT, end='')
 
     @staticmethod
-    def printSuccess(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
+    def success(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
         print(BeautifulPrint.GREEN, end='')
         print(msg, sep=sep, end=end, file=file, flush=flush)
         print(BeautifulPrint.END_PRINT, end='')
     
     @staticmethod
-    def printBold(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
+    def bold(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
         print(BeautifulPrint.BOLD, end='')
         print(msg, sep=sep, end=end, file=file, flush=flush)
         print(BeautifulPrint.END_PRINT, end='')
         
     @staticmethod
-    def printUnderline(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
+    def underline(msg, sep=' ', end='\n', file=sys.stdout, flush=False):
         print(BeautifulPrint.UNDERLINE, end='')
         print(msg, sep=sep, end=end, file=file, flush=flush)
         print(BeautifulPrint.END_PRINT, end='')
 
 if __name__ == '__main__':
     # Some preset functions
-    BeautifulPrint.printWarning("Warning")
-    BeautifulPrint.printInfoBlue("Information Blue")
-    BeautifulPrint.printInfoPurple("Information Purple")
-    BeautifulPrint.printError("Error")
-    BeautifulPrint.printSuccess("Success")
-    BeautifulPrint.printUnderline("Underline")
-    BeautifulPrint.printBold("Bold")
+    BeautifulPrint.warning("Warning")
+    BeautifulPrint.infoBlue("Information Blue")
+    BeautifulPrint.infoPurple("Information Purple")
+    BeautifulPrint.error("Error")
+    BeautifulPrint.success("Success")
+    BeautifulPrint.underline("Underline")
+    BeautifulPrint.bold("Bold")
 
     # For more complicated combination, do it manually
     print(BeautifulPrint.UNDERLINE, BeautifulPrint.BOLD, BeautifulPrint.GREEN, sep='', end='')
