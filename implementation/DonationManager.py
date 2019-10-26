88057558d31d50afffbe543d0c50a8c001e4a78c
Webster Zhang <wencatzhang@gmail.com>
2019-10-26 22:24:31 +1100

    Refactoring and extract JsonWriter class

--- implementation/DonationManager.py
+++ implementation/DonationManager.py
@@ -1,4 +1,5 @@
 from BeautifulPrint import BeautifulPrint
+from JsonWriter import JsonWriter
 from MenuLevel import MenuLevel
 from Role import Role
 from UntestedBlood import UntestedBlood
@@ -22,38 +23,34 @@ class DonationManager(Role):
         
     def insertBlood(self):
         newBlood = UntestedBlood(int(time.time()))
-        untestedList = []
 
-        open('./dataset/UntestedBlood.json', 'a')
+        untestedList = JsonWriter.parseJsonFromFile(
+            filename='./dataset/UntestedBlood.json', 
+            defaultValue=[]
+        )
 
-        with open('./dataset/UntestedBlood.json', 'r+') as f:
-            try:
-                untestedList = json.load(f)
-            except:    
-                untestedList = []
-            finally:
-                untestedList.append(newBlood.toDictionary())
+        untestedList.append(newBlood.toDictionary())
         
-        with open('./dataset/UntestedBlood.json', 'w') as f:
-            json.dump(untestedList, f)
+        JsonWriter.writeJsonToFile(
+            filename='./dataset/UntestedBlood.json',
+            data=untestedList
+        )
 
         BeautifulPrint.success('Insert ' + str(newBlood) + ' successed. Please insert the blood to the storehouse')
         input('Press enter to go back...')
 
 
     def viewBlood(self):
-        open('./dataset/UntestedBlood.json', 'a')
+        untestedList = JsonWriter.parseJsonFromFile(
+            filename='./dataset/UntestedBlood.json', 
+            defaultValue=[]
+        )
 
-        with open('./dataset/UntestedBlood.json', 'r') as f:
-            try:
-                untestedList = json.load(f)
-            except:
-                untestedList = []
-            finally:
-                print('We currently have ', end='')
-                BeautifulPrint.bold(str(len(untestedList)), end='')
-                print(' untested blood.')
-                for blood in untestedList:
-                    BeautifulPrint.infoPurple(str(UntestedBlood(blood['id'])))
+        print('We currently have ', end='')
+        BeautifulPrint.bold(str(len(untestedList)), end='')
+        print(' untested blood.')
+        
+        for blood in untestedList:
+            BeautifulPrint.infoPurple(str(UntestedBlood(blood['id'])))
 
         input('Press enter to go back...')
