35e3fcb9a32bb9ea56494b7ebfa3e13936f116e6
pingchoy <pingchoy.1@gmail.com>
2019-11-03 19:17:23 +1100

    Doctors can reserve blood

--- implementation/ReservedBloodList.py
+++ implementation/ReservedBloodList.py
@@ -3,14 +3,14 @@ from BloodList import BloodList
 from TestedBlood import TestedBlood
 from JsonWriter import JsonWriter
 
-class TestedBloodList(BloodList):
-    jsonfile = './dataset/TestedBlood.json'
+class ReservedBloodList(BloodList):
+    jsonfile = './dataset/ReservedBlood.json'
 
     def __init__(self):
         super().__init__()
         self.list = []
         data = JsonWriter.parseJsonFromFile(
-            filename = self.jsonfile, 
+            filename = self.jsonfile,
             defaultValue = []
         )
         for blood in data:
