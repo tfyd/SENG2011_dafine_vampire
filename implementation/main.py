9e183eecb6220675171e0adffaabafcc933c887e
yahniszheng <yang.zheng@student.unsw.edu.au>
2019-11-07 23:58:20 +1100

    add clean screen

--- implementation/main.py
+++ implementation/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from BeautifulPrint import BeautifulPrint
 from VampireSystem import VampireSystem
+import os
 
 def main():
     yellow = BeautifulPrint.warning
     red = BeautifulPrint.error
-
+    os.system('clear') # clear the screen
     yellow('===================================')
     yellow('    Welcome to Project ', end='')
     red('Vampire', end='')
